c760a94a093af352160e8447a4328d45a3926d33
submissoesfpbf-dotcom <submissoes.fpbf@gmail.com>
2026-09-17 17:04:38 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.III_FPBF_CONFIG = {
+  APP_NAME: 'III FPBF - Credenciamento',
+  VERSION: '4.0.0-pwa',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxmjX8cIrxBwBU-D3HGuZYG3Jmnb2FshOH7j2y-hnDSfDcO7vfHn8Z5HO30e00RmbJhqg/exec',
+  API_KEY: 'IIIFPBF-2026-CRED-9X7K4P',
+  DUPLICATE_SECONDS: 10
+};
